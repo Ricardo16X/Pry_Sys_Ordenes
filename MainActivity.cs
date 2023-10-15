24a640f31bfa9eb3c1d69c67c35cf32f91ca1eb3
Ricardo Pérez <38416505+Ricardo16X@users.noreply.github.com>
2023-10-15 16:19:00 -0600

    Agregar archivos de proyecto.

---
+++ MainActivity.cs
@@ -0,0 +1,15 @@
+namespace VentasQuincenales
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            // Realiza una operación de suma
+        }
+    }
+}
